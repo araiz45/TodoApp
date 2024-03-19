551403d9c89bcefafc00576f344805f7cf6182ea
araiz45 <araizzahid45@gmail.com>
2024-03-19 23:42:37 +0500

    login completd

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,36 @@
+"use server";
+import mongoDB from "@/db/db";
+import { NextResponse } from "next/server";
+import User from "@/models/User";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+mongoDB();
+export async function POST(req: { json: () => any }) {
+  const secret = process.env.JWTSECRET || "LSDJF;SDLFJ";
+  const { username, password } = await req.json();
+  console.log(username, password);
+  try {
+    const existedUser = await User.find({ name: username });
+    console.log(existedUser[0]);
+    const comparePassword = await bcrypt.compare(
+      password,
+      existedUser[0].password
+    );
+    console.log(comparePassword);
+    if (!comparePassword) {
+      return NextResponse.json({ message: "bad reqests" }, { status: 400 });
+    }
+    const payload = {
+      name: existedUser[0].name,
+      id: existedUser[0]._id,
+    };
+    const token = jwt.sign(payload, secret, {
+      expiresIn: "1h",
+    });
+    cookies().set("token", token);
+    return NextResponse.json({ message: "done" });
+  } catch (error) {
+    return NextResponse.json({ message: "wrong" }, { status: 500 });
+  }
+}
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,39 @@
+"use client";
 import Link from "next/link";
-
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { push } = useRouter();
+  const handleSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+    const data = {
+      username,
+      password,
+    };
+    try {
+      const response = await fetch(process.env.serverUrl + "/login", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "content-type": "application/json",
+        },
+      });
+      console.log(response);
+      setUsername("");
+      setPassword("");
+      push("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <section className="bg-gray-300 min-h-screen flex justify-center items-center">
-      <form className="flex flex-col gap-1 bg-white py-10 px-20 rounded-md shadow-lg">
+      <form
+        className="flex flex-col gap-1 bg-white py-10 px-20 rounded-md shadow-lg"
+        onSubmit={handleSubmit}
+      >
         <h1 className="text-4xl font-bold text-black text-center mb-4">
           Login
         </h1>
@@ -14,6 +44,8 @@ export default function Login() {
           type="text"
           className="border-black border-2 py-2 px-3 outline-none rounded-md"
           name="name"
+          value={username}
+          onChange={(ev) => setUsername(ev.target.value)}
         />
         <label htmlFor="name" className="">
           Enter Password
@@ -23,6 +55,8 @@ export default function Login() {
           name=""
           id=""
           className="border-black border-2 py-2 px-3 outline-none rounded-md"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
         />
         <button
           type="submit"
