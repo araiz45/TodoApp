56d4855d1954cd1082eb956954483713ad1c1cc6
araiz45 <araizzahid45@gmail.com>
2024-03-20 01:01:14 +0500

    final pro

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 export default async function mongoDB() {
   try {
     if (process.env.mongoUrl !== undefined) {
-      const connection = await mongoose.connect(process.env.mongoUrl);
+      const connection = await mongoose.connect(
+        process.env.mongoUrl ||
+          "mongodb+srv://araiz:araiz@cluster0.mn5uyv5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      );
       console.log("Connection Successfull");
     }
   } catch (error) {
