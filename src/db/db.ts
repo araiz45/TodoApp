93947f6e7aeba7d4a912ac69a6c017f450cd1a02
araiz45 <araizzahid45@gmail.com>
2024-03-20 01:03:55 +0500

    good

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,8 +4,7 @@ export default async function mongoDB() {
   try {
     if (process.env.mongoUrl !== undefined) {
       const connection = await mongoose.connect(
-        process.env.mongoUrl ||
-          "mongodb+srv://araiz:araiz@cluster0.mn5uyv5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        "mongodb+srv://araiz:araiz@cluster0.mn5uyv5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
       );
       console.log("Connection Successfull");
     }
