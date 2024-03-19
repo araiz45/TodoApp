e3dba699f55388d79b8d8ec6d104e15b5fb6f9a9
araiz45 <araizzahid45@gmail.com>
2024-03-20 00:36:25 +0500

    completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
 import Login from "@/components/Login";
 import Register from "@/components/Register";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <Register />
-    </>
+    <section
+      className="min-h-screen bg-center bg-cover bg-no-repeat flex justify-center flex-col"
+      style={{ background: "url('/bg.jpg')" }}
+    >
+      <h1 className="text-white text-7xl font-bold leading-relaxed">
+        Take Notes <span className="bg-rose-700">So</span> <br />
+        You Can Remember <br />
+        Everything
+      </h1>
+      <div className="flex items-center gap-3 w-[33rem]">
+        <Link
+          href={"/login"}
+          className="bg-transparent border-2 border-rose-700 text-rose-700 py-2 px-3 rounded-md flex justify-center items-center w-1/2 hover:bg-rose-700 transition-colors duration-100 hover:text-white"
+        >
+          Login
+        </Link>
+        <Link
+          href={"/register"}
+          className="bg-transparent border-2 border-rose-700 text-rose-700 py-2 px-3 rounded-md flex justify-center items-center w-1/2 hover:bg-rose-700 transition-colors duration-100 hover:text-white"
+        >
+          Register
+        </Link>
+      </div>
+    </section>
   );
 }
