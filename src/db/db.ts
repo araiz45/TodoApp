fbcaac74e7a245a66e5dab6ec864d2c4792e468c
araiz45 <araizzahid45@gmail.com>
2024-03-20 01:42:50 +0500

    mongodb url

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 export default async function mongoDB() {
   try {
     if (process.env.mongoUrl !== undefined) {
-      const connection = await mongoose.connect(
-        "mongodb+srv://araiz:araiz@cluster0.mn5uyv5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-      );
+      const connection = await mongoose.connect(process.env.mongoUrl);
       console.log("Connection Successfull");
     }
   } catch (error) {
